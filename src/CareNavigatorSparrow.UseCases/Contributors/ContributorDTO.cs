5e4e2ff5156f1d4cfc9976ccaaedd231bf90a9a8
Alex McCool <alex.mccool@gmail.com>
2025-01-14 13:55:46 -0600

    template project for clean-arch

---
+++ src/CareNavigatorSparrow.UseCases/Contributors/ContributorDTO.cs
@@ -0,0 +1,2 @@
+﻿namespace CareNavigatorSparrow.UseCases.Contributors;
+public record ContributorDTO(int Id, string Name, string? PhoneNumber);
---
+++ src/CareNavigatorSparrow.Web/Configurations/ServiceConfigs.cs
@@ -0,0 +1,36 @@
+﻿using CareNavigatorSparrow.Core.Interfaces;
+using CareNavigatorSparrow.Infrastructure;
+using CareNavigatorSparrow.Infrastructure.Email;
+
+namespace CareNavigatorSparrow.Web.Configurations;
+
+public static class ServiceConfigs
+{
+  public static IServiceCollection AddServiceConfigs(this IServiceCollection services, Microsoft.Extensions.Logging.ILogger logger, WebApplicationBuilder builder)
+  {
+    services.AddInfrastructureServices(builder.Configuration, logger)
+            .AddMediatrConfigs();
+
+
+    if (builder.Environment.IsDevelopment())
+    {
+      // Use a local test email server
+      // See: https://ardalis.com/configuring-a-local-test-email-server/
+      services.AddScoped<IEmailSender, MimeKitEmailSender>();
+
+      // Otherwise use this:
+      //builder.Services.AddScoped<IEmailSender, FakeEmailSender>();
+
+    }
+    else
+    {
+      services.AddScoped<IEmailSender, MimeKitEmailSender>();
+    }
+
+    logger.LogInformation("{Project} services registered", "Mediatr and Email Sender");
+
+    return services;
+  }
+
+
+}
